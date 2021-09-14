72ede181085f3687d0b756dc995c03493245fc35
MARYEM-BAIZ <baiz.maryem2018@gmail.com>
2021-09-14 23:49:14 +0200

    commit 1

---
+++ resources/views/template.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{{asset('accueil.css')}}">
+    <title>@yield('pagename')</title>
+</head>
+<body>
+    <header>
+        <article></article>
+        <article> <h3>Etudiant</h3> </article>
+        <article></article>
+    </header>
+
+    <main>
+        <p>hello</p>
+    </main>
+
+    <footer></footer>
+</body>
+</html>
