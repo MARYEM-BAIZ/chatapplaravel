d105f2e64674eb83558b3a786ef1083c522fea55
MARYEM-BAIZ <baiz.maryem2018@gmail.com>
2021-09-29 18:51:42 +0200

    commit

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -16,22 +16,27 @@ Accueil
                         <div class="profile-pic"><img src="" alt=""></div>
                         <p class="username">modern_web_channel</p>
                     </div>
-                    <img src="" class="options" alt="">
+                    <a href=""><i class="fa fa-ellipsis-h"></i></a>
                 </div>
                 <img src="" class="post-image" alt="">
                 <div class="post-content">
                     <div class="reaction-wrapper">
-                        <img src="" class="icon" alt="">
-                        <img src="" class="icon" alt="">
-                        <img src="" class="icon" alt="">
-                        <img src="" class="save icon" alt="">
+                   <div class="container ">
+                       <div class="row">
+                       <div class="col-2"><a  href=""><i class="fab fa-gratipay "></i></a></div>
+                       <div class="col-2"></div>
+                        <div class="col-2"><a  href=""><i class="far fa-comments"></i></a></div>
+                        <div class="col-2"></div>
+                       <div class="col-2"><a  href=""><i class="far fa-share-square"></i></a></div>
+                       </div>
+                   </div>
                     </div>
                     <p class="likes">1,012 likes</p>
                     <p class="description"><span>username </span> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur tenetur veritatis placeat, molestiae impedit aut provident eum quo natus molestias?</p>
-                    <p class="post-time">2 minutes ago</p>
+                     <p class="post-time">2 minutes ago</p>
                 </div>
                 <div class="comment-wrapper">
-                    <img src="" class="icon" alt="">
+                    <a href=""></a>
                     <input type="text" class="comment-box" placeholder="Add a comment">
                     <button class="comment-btn">post</button>
                 </div>
