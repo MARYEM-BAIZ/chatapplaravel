fa0c0250455ba4f6769c08b01024f86f8550c551
MARYEM-BAIZ <baiz.maryem2018@gmail.com>
2021-10-01 12:44:54 +0200

    commit

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,29 +1,43 @@
-@extends('template')
-@section('title')
-Login
-@endsection
-
-@section('contenu')
-<div class="container">
-    <div class="row">
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>Material Design for Bootstrap</title>
+    <!-- MDB icon -->
+    <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon" />
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" />
+    <!-- Google Fonts Roboto -->
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"
+    />
+    <!-- MDB -->
+    <link rel="stylesheet" href="css/mdb.min.css" />
+  </head>
+  <body>
+   
+  <div style="margin-top:92px" class="container  ">
+    <div class="row ">
       <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
         <div class="card border-0 shadow rounded-3 my-5">
           <div class="card-body p-4 p-sm-5">
-            <h5 class="card-title text-center mb-4 fw-light fs-5">Sign In</h5>
+            <h5  class="card-title text-center mb-4 fw-light ">Se connecter</h5>
             <form>
             <div class="mb-3">
-            <label class="labels mb-2">Email address</label><input style=" border-radius: 30px;"  type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <label style="font-size: 0.9rem;" class="labels mb-2">Adresse Email</label><input style=" border-radius: 30px; padding: 10px;"  type="email" class="form-control" id="floatingInput" >
             </div>
               
 
             <div class="mb-3">
-            <label class="labels mb-2">Password</label><input style=" border-radius: 30px;" type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <label style="font-size: 0.9rem;" class="labels mb-2">Mot de passe</label><input style=" border-radius: 30px; padding: 10px;" type="password" class="form-control" id="floatingPassword" >
             </div>
               
 
               <div class="d-grid">
-                <button style="" class="btn" type="submit">Sign
-                  in</button>
+                <button style="" class="btnlogin" type="submit">Se connecter</button>
               </div>
              
             </form>
@@ -32,4 +46,9 @@ Login
       </div>
     </div>
   </div>
-@endsection
+    <!-- MDB -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+    <!-- Custom scripts -->
+    <script type="text/javascript"></script>
+  </body>
+</html>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,47 +1,62 @@
-@extends('template')
-@section('title')
-Création du compte
-@endsection
-
-@section('contenu')
-<div class="container">
-    <div class="row">
-      <div class="col-lg-10 col-xl-9 mx-auto">
-        <div class="card flex-row my-5 border-0 shadow rounded-3 overflow-hidden">
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>Material Design for Bootstrap</title>
+    <!-- MDB icon -->
+    <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon" />
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" />
+    <!-- Google Fonts Roboto -->
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"
+    />
+    <!-- MDB -->
+    <link rel="stylesheet" href="css/mdb.min.css" />
+  </head>
+  <body>
+   
+  <div class="container">
+    <div class="row"> 
+      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <div class="card border-0 shadow rounded-3 my-5">
           <div class="card-body p-4 p-sm-5">
-            <h5 class="card-title text-center mb-5 fw-light fs-5">Register</h5>
+            <h5 class="card-title text-center mb-5 fw-light fs-5">S'inscrire</h5>
             <form>
 
-              <div class="form-floating mb-3">
-                <input type="text" class="form-control" id="floatingInputUsername" placeholder="myusername" required autofocus>
-                <label for="floatingInputUsername">Username</label>
-              </div>
-
-              <div class="form-floating mb-3">
-                <input type="email" class="form-control" id="floatingInputEmail" placeholder="name@example.com">
-                <label for="floatingInputEmail">Email address</label>
-              </div>
-
-              <hr>
-
-              <div class="form-floating mb-3">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                <label for="floatingPassword">Password</label>
-              </div>
-
-              <div class="form-floating mb-3">
-                <input type="password" class="form-control" id="floatingPasswordConfirm" placeholder="Confirm Password">
-                <label for="floatingPasswordConfirm">Confirm Password</label>
-              </div>
-
-              <div class="d-grid mb-2">
-                <button class="btn btn-lg btn-primary btn-login fw-bold text-uppercase" type="submit">Register</button>
-              </div>
-
-              <a class="d-block text-center mt-2 small" href="#">Have an account? Sign In</a>
-
-
+            <div class="mb-3">
+            <label style="font-size: 0.9rem;" class="labels mb-2">Username</label><input style=" border-radius: 30px; padding: 10px;"  type="text" class="form-control" id="floatingInput" >
+            </div>
              
+
+            <div class="mb-3">
+            <label style="font-size: 0.9rem;" class="labels mb-2">Adresse Email</label><input style=" border-radius: 30px; padding: 10px;"  type="email" class="form-control" id="floatingInput" >
+            </div>
+           
+
+
+            <div class="mb-3">
+            <label style="font-size: 0.9rem;" class="labels mb-2">Mot de passe</label><input style=" border-radius: 30px; padding: 10px;"  type="password" class="form-control" id="floatingInput" >
+            </div>
+             
+
+
+            <div class="mb-3">
+            <label style="font-size: 0.9rem;" class="labels mb-2">Confirmer le mot de passe</label><input style=" border-radius: 30px; padding: 10px;"  type="password" class="form-control" id="floatingInput" >
+            </div>
+              
+
+
+
+              <div class="d-grid">
+                <button class="btnregister" type="submit">S'inscrire</button>
+              </div>
+
+              <a class="d-block text-center mt-2 small" href="#">Vous avez un compte? Se connecter</a>
+
 
         
 
@@ -51,4 +66,10 @@ Création du compte
       </div>
     </div>
   </div>
-@endsection
+
+    <!-- MDB -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+    <!-- Custom scripts -->
+    <script type="text/javascript"></script>
+  </body>
+</html>
